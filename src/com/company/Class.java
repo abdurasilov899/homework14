92bc7005b0b7b0ce4c7993d6bd09a7b5ba4927cb
User <abdurasilov99@gmail.com>
2022-02-07 01:54:31 -0800

    Initial commit

---
+++ src/com/company/Class.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Class {
+    private String name;
+    private int nomer;
+    private int[] massiva;
+
+    public Class() {
+
+    }
+
+    public Class(String name, int nomer, int[] massiva) {
+        this.name = name;
+        this.nomer = nomer;
+        this.massiva = massiva;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNomer() {
+        return nomer;
+    }
+
+    public void setNomer(int nomer) {
+        this.nomer = nomer;
+    }
+
+    public int[] getMassiva() {
+        return massiva;
+    }
+
+    public void setMassiva(int[] massiva) {
+        this.massiva = massiva;
+    }
+}
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Class talenst = new Class("Kuba", 23, new int[]{12, 43, 12, 54});
+        System.out.println(talenst.getName() + " " + talenst.getNomer() + " " + Arrays.toString(talenst.getMassiva()));
+    }
+}
